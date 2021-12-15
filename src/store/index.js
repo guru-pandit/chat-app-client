4b53360b3fad47a8a245a62408e472a40989bc1c
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-12-15 19:44:00 +0530

    added redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
+
 import rootReducer from "../reducers";
 
-const middleware = applyMiddleware(thunk);
+const middlewares = [logger, thunk];
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // creatung store by passing all reducers
-const store = createStore(rootReducer, composeEnhancer(middleware));
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)));
 
 export default store;
