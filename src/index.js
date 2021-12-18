647fff336bfe0cde90877abe6c683d335032cc1f
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-12-18 21:21:25 +0530

    added persist store

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import store from './store';
+import { store, persistor } from './store';
+
+import { Loader } from "./components";
 
 import "./styles/tailwind.css";
 import "./styles/custom.css";
@@ -15,10 +18,12 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+    <PersistGate loading={<Loader />} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider >,
   document.getElementById('root')
 );
 reportWebVitals();
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 
 import rootReducer from "../reducers";
+
+const persistConfig = {
+    key: "root",
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger, thunk];
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // creatung store by passing all reducers
-const store = createStore(rootReducer, composeEnhancer(applyMiddleware(...middlewares)));
+const store = createStore(persistedReducer, composeEnhancer(applyMiddleware(...middlewares)));
+// Persistor store
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
